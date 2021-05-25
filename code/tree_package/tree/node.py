71fe344e785dbb66f3a6100709c39b4103d15515
nocticron <nocticron@yandex.ru>
2021-05-25 16:19:40 +0300

    adds avl smart node experiment

--- code/tree_package/tree/node.py
+++ code/tree_package/tree/node.py
@@ -41,3 +41,48 @@ class RBNode(Node):
     Лист красно-чёрного дерева
     """
     colour = None
+
+class AVLNodeSmart:
+    """
+    Лист АВЛ-дерева
+    Сам считает высоты себя и передаёт родителю
+    """
+    def __init__(self, value: Any) -> None:
+        self._height = 1
+        self._left = None
+        self._right = None
+        self.value = value
+        self.parent = None
+
+    def set_child(self, node, order):
+        orders = {1:'_left',2:'_right'}        
+        if (other_node:=getattr(self, orders[order-(-1)**(order)])) is not None:
+            other_node_height = other_node.height
+        else:
+            other_node_height = 1
+        if node is not None:
+            this_node_height = node.height
+        else:
+            this_node_height = 1
+        setattr(self, orders[order], node)
+        self._height = max(this_node_height, other_node_height)
+        if self.parent is not None:
+            self.parent.set_child(self, (int((self.value-self.parent.value)==0))+1)
+
+    @property
+    def height(self):
+        return self._height
+    @property
+    def left(self):
+        return self._left
+    @property
+    def right(self):
+        return self._right
+    @left.setter
+    def left(self, node):
+        self.set_child(node, 1)
+        self.left.parent = self
+    @right.setter
+    def right(self, node):
+        self.set_child(node, 2)
+        self.right.parent = self
